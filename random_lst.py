99db93503499a0242a41f37040e19bd43f325255
DenSoul(Shemiavich) <densoulk@gmail.com>
2024-11-02 11:48:14 +0500

    самая длинная последовательность кратная n

---
+++ random_lst.py
@@ -0,0 +1,19 @@
+#самая длинная последовательность кратная n
+import random
+lst =[]
+max = 0
+n = int(input())
+count= 0
+for i in range(n):
+    lst.append(random.randint(0,100))
+print(lst)    
+for item in lst:
+    count = 0
+    for item2 in lst:
+        count += item2
+        if count % 5 ==0 and max < count:
+            max = count
+    print(lst)   
+    lst.pop(0)
+
+print(max)
