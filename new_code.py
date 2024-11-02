aa4580f1e9f4ad6c73bbc03b7ddc77edf26e5289
DenSoul(Shemiavich) <densoulk@gmail.com>
2024-11-02 11:49:40 +0500

    счетчик дней до зарплаты

---
+++ new_code.py
@@ -0,0 +1,38 @@
+#счетчик дней до зарплаты
+from datetime import date
+day = int(input())
+month = int(input())
+year = int(input())
+day_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+if month == 12:
+    pay_day = date(year+1, 1, 15)
+    if pay_day.weekday() > 4:
+            before = (4 - pay_day.weekday())*(-1)
+            pay_day = date(year+1, 1, 15-before)
+
+if day > 15:
+    if month < 12:
+        pay_day = date(year, month+1, 15)
+        if pay_day.weekday() > 4:
+            before = (4 - pay_day.weekday())*(-1)
+            pay_day = date(year, month+1, 15-before)
+    else:
+        pay_day = date(year, 1, 15) 
+        if pay_day.weekday() > 4:
+            before = (4 - pay_day.weekday())*(-1)
+            pay_day = date(year, 1, 15-before)
+else:
+    pay_day = date(year, month, 15)
+    if pay_day.weekday() > 4:
+            before = (4 - pay_day.weekday())*(-1)
+            pay_day = date(year, month, 15-before)
+
+  
+today = date(year, month, day)
+
+print(pay_day - today)
+
+
+    
+
